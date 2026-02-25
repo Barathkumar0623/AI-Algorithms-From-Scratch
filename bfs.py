15745a589e9cc212738a4f14d6faceef99c31193
Barathkumar0623 <billabarath578@gmail.com>
2026-02-25 14:02:01 +0530

    First commit - Added BFS

---
+++ bfs.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def bfs(graph, start):
+    visited = []
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            queue.extend(graph[node])
+
+    return visited
+
+result = bfs(graph, 'A')
+print("BFS Traversal:", result)
